4d8d1c144704b919e49adf29de1549403e63b71f
himanshp1656 <126962282+himanshp1656@users.noreply.github.com>
2023-07-21 13:16:51 -0700

    Update settings.py

--- blognet/settings.py
+++ blognet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_vy_+6^b99-9z=@o078r7(qzkscn(op)hgj9r2bex%ci2e%5j@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
