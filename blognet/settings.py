7c500f66fcac04f92ef33efb30a0448f30b3ef4e
himanshp1656 <126962282+himanshp1656@users.noreply.github.com>
2023-07-23 06:03:50 -0700

    Update settings.py

--- blognet/settings.py
+++ blognet/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR,'static'),
 ]
