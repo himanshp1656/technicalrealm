669587de10c672d2f00f6f33701288b065d77914
himanshp1656 <126962282+himanshp1656@users.noreply.github.com>
2023-10-14 19:58:24 +0530

    Update settings.py

--- blognet/settings.py
+++ blognet/settings.py
@@ -78,12 +78,25 @@ WSGI_APPLICATION = 'blognet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / "db.sqlite3"),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / "db.sqlite3"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'qCvIyq1YvlMx8rjXsqN0',
+        'HOST':'containers-us-west-121.railway.app',
+        'PORT':'6463',
     }
 }
+
 
 # DATABASES = {
 #     'default': {
@@ -124,7 +137,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
